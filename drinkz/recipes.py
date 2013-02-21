47843443292f9d1f90effbbb0027d9fd1ca10691
Anthony Bird <birdanth@adriatic.cse.msu.edu>
2013-02-20 22:51:02 -0500

    recipes added

---
+++ drinkz/recipes.py
@@ -0,0 +1,19 @@
+#recipe class used to store recipes in db.py
+from . import db
+
+class Recipe:
+    	
+    def __init__(self,name,ing):
+	self.name = name
+        self.ing = ing 
+
+    def __len__(self):
+        return len(self.__recipes)
+
+    def add(self,a):
+        self.ing.append(a)
+
+    def need_ingredients(r):
+	for item in r:
+	    yield item
+
