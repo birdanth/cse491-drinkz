8d5799cee4e1fa945514806f8c1e3343ad69e7f8
Anthony Bird <birdanth@adriatic.cse.msu.edu>
2013-04-15 03:31:24 -0400

    3:30am - I hate merging on git

--- drinkz/unitconversion.py
+++ drinkz/unitconversion.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #unit conversion
 
 def convert_to_ml(amount):
@@ -18,25 +17,3 @@ def convert_to_ml(amount):
 		print 'unknown unit type, not added to total'
 
 	return num
-=======
-#unit conversion
-
-def convert_to_ml(amount):
-<<<<<<< HEAD
-	num, units = amount.split()
-	num = float(num)
-	units = units.lower()
-
-	if units == 'ml':
-		pass
-	elif units == 'liters' or units == 'liter':
-		num = 1000.0*num
-	elif units == 'oz':
-		num = 29.5735*num
-	elif units == 'gallons' or units == 'gallon' or units == 'gall':
-		num = 3785.41*num
-	else:
-		print 'unknown unit type, not added to total'
-
-	return num
->>>>>>> 3d9fc6de9e2c6eb07bdc034df8871d41ddb01a0b
